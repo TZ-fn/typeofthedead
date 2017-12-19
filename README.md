52612372e089dfbf12960dec8a1318c3dfbc7228
Tomasz Ziolkowski <tomekz@vp.pl>
2017-12-19 17:29:36 +0100

    Added basic functionality.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Type of the Dead :skull:
-#### Simple typing game written in JavaScript to pratice JS and working with DOM, while using GIT.
-#### I used [Meshki](https://borderliner.github.io/Meshki/) framework.
+Simple typing game written in JavaScript to pratice JS and working with DOM, while using GIT.
+I used [Meshki](https://borderliner.github.io/Meshki/) framework.
--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 'use strict';
 window.onload = function () {
-  const words = ['', '', '', '', '', ''];
+  const words = ['red', 'let', 'young', 'mother', 'pristine', 'haphazard', 'masterful', 'diligently', 'euphonious', 'jargonizing', 'zygapophyses', 'abbreviations', 'infrastructure', 'acknowledgement'],
+    wordBox = document.querySelector('#word'),
+    wordInput = document.querySelector('#word-input'),
+    resultBox = document.querySelector('#result'),
+    startBtn = document.querySelector('#startBtn'),
 
+    startGame = () => {
+      let i = 0,
+        text = '';
+      wordBox.innerHTML = '&nbsp;' + words[i];
+      wordInput.value = '';
+      resultBox.textContent = '';
+      startBtn.textContent = 'Restart';
+      wordInput.addEventListener('input', checkInput);
+
+      function checkInput() {
+        if (wordInput.value === words[i]) {
+          resultBox.innerHTML = '&nbsp;Correct!';
+          resultBox.classList = 'correct';
+          i++;
+          wordInput.value = '';
+          wordBox.innerHTML = '&nbsp;' + words[i];
+        } else {
+          resultBox.innerHTML = '&nbsp;Incorrect!';
+          resultBox.classList = 'incorrect';
+        }
+      };
+    };
+  startBtn.addEventListener('click', startGame);
 };
